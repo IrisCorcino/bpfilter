f06f6becc309b32b325c7e7b557d87089c5dc1b6
dmarty <dmarty@hsr.ch>
2018-06-18 22:48:25 +0200

    added sample count to csv plotter

--- startMeasurements.sh
+++ startMeasurements.sh
@@ -1,22 +1,24 @@
 print_info_and_exit() {
   echo "Usage:"
-  echo "$0 -m MEASUREMENT OUTPUT MEASUREMENT_IN_SEC" >&2
+  echo "$0 -m MEASUREMENT OUTPUT MEASUREMENT_IN_SEC (CHANNEL | CHANNEL_FROM CHALLEN_TO)" >&2
   echo "   MEASUREMENT" >&2
   echo "      0 - ALL" >&2
   echo "      1 - SNR" >&2
   echo "      2 - PS" >&2
   echo "or" >&2
-  echo "$0 -p PLOTTER OUTPUT INPUT FILE_PATTERN" >&2
+  echo "$0 -p PLOTTER OUTPUT INPUT FILE_PATTERN (FILE_PATTERN | FILE_PATTERN FILE_PATTERN)" >&2
   echo "   PLOTTER" >&2
   echo "      0 - CSV" >&2
   echo "      1 - DIAGRAM" >&2
   echo "      2 - DIFF DIAGRAM" >&2
+  echo "   FILE_PATTERN" >&2
+  echo "      for <name>_<id>_<freq>.bin the FILE_PATTERN is <name>" >&2
   exit 1
 }
 
 check_if_number() {
   if [[ $(($1)) != $1 ]]; then
-    echo "argument is not a number" >&s
+    echo "argument is not a number" >&2
     print_info_and_exit
   fi
 }
@@ -28,12 +30,18 @@ check_if_existing_directory() {
   fi
 }
 
-source ~/pybombs/prefix/default/setup_env.sh
+SETUP_DIR1="~/pybombs/prefix/default/setup_env.sh"
+SETUP_DIR2="~/pybombs/prefix/setup_env.sh"
+if [ -f $SETUP_DIR1 ]; then
+		source $SETUP_DIR1
+else
+		source $SETUP_DIR2
+fi
 
 case "$1" in
 "-m")
-  # -m MEASUREMENT OUTPUT MEASUREMENT_IN_SEC
-  if [ $# != 4 ]; then
+  # -m MEASUREMENT OUTPUT MEASUREMENT_IN_SEC (CHANNEL | CHANNEL_FROM CHANNEL_TO)
+  if [ $# -lt 4 ] || [ $# -gt 6 ]; then
     echo "incorrect number of arguments" >&2
     print_info_and_exit
   fi
@@ -41,20 +49,30 @@ case "$1" in
   check_if_existing_directory "$3"
   check_if_number "$4"
 
+	if [ $# -eq 5 ]; then
+		check_if_number "$5"
+	fi
+
+  if [ $# -eq 6 ]; then
+		check_if_number "$6"
+	fi
+
   MEASUREMENT=$2
   OUTPUT_DIR=$3
   MEASUREMENT_IN_SEC=$4
+	CHANNEL_FROM=$5
+	CHANNEL_TO=$6
 
   case "$MEASUREMENT" in
   "0")
-    python2 ./signal_to_noise_ratio/SNRMeasurementStarter.py $OUTPUT_DIR $MEASUREMENT_IN_SEC 
-    python2 ./potency_of_signal/PSMeasurementStarter.py $OUTPUT_DIR $MEASUREMENT_IN_SEC 
+    python2 ./signal_to_noise_ratio/SNRMeasurementStarter.py $OUTPUT_DIR $MEASUREMENT_IN_SEC $CHANNEL_FROM $CHANNEL_TO
+    python2 ./potency_of_signal/PSMeasurementStarter.py $OUTPUT_DIR $MEASUREMENT_IN_SEC $CHANNEL_FROM $CHANNEL_TO
     ;;
   "1")
-    python2 ./signal_to_noise_ratio/SNRMeasurementStarter.py $OUTPUT_DIR $MEASUREMENT_IN_SEC  
+    python2 ./signal_to_noise_ratio/SNRMeasurementStarter.py $OUTPUT_DIR $MEASUREMENT_IN_SEC $CHANNEL_FROM $CHANNEL_TO
     ;;
   "2")
-    python2 ./potency_of_signal/PSMeasurementStarter.py $OUTPUT_DIR $MEASUREMENT_IN_SEC 
+    python2 ./potency_of_signal/PSMeasurementStarter.py $OUTPUT_DIR $MEASUREMENT_IN_SEC $CHANNEL_FROM $CHANNEL_TO
     ;;
   *)
     print_info_and_exit
@@ -89,6 +107,6 @@ case "$1" in
   exit 0
   ;;
  *)
-  echo "incorrect option"
+  echo "incorrect option" >&2
   print_info_and_exit
  esac
